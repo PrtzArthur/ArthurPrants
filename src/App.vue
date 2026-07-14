88d4eeafef5503ee7139296aa9b31cb5301fa28a
PrtzArthur <prantsarthur2010@gmail.com>
2026-07-14 09:25:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { routerView } from 'vue-router'
+import AppHeader from './components/layout/AppHeader.vue'
+</script>
+
+<template> 
+  <AppHeader />
+  <routerView />
+
+</template>
+
+<style scoped>
+</style>
